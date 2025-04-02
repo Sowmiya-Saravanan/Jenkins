98a8c4e4b0efce7bb4aeb48f9eaca57f6b89386c
Sowmiya <ssowmiyasaravananr@gmail.com>
2025-04-02 12:07:55 +0530

    Initial commit

---
+++ src/test/java/com/jenkin/jenkindemo/JenkindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkin.jenkindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkindemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
